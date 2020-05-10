62446e843d60feee9b410e69de47ac2da9b374ca
Kevinclane <kevinclane21@gmail.com>
2020-05-10 15:30:23 -0600

    Desktop finished

--- app.js
+++ app.js
@@ -31,10 +31,17 @@ let idle = {
   wood: 0
 }
 
+let totalWorkers = 0
+let timerCounter = 0
+let maxFood = 0
+let maxOre = 0
+let maxWood = 0
 
 
 
 let upgradeArr = ['Stone', 'Bronze', 'Iron', 'Steel', 'Diamond']
+
+//#region DRAW PAGE ELEMENTS
 
 //Loads info onto the page
 function draw() {
@@ -67,6 +74,12 @@ function drawUserInfo() {
 
   document.getElementById("woodPerClick").innerText = "Wood Per Click: " + (user.mods.wood + 1)
 
+  document.getElementById("workers").innerText = "Total Workers: " + totalWorkers + " Upkeep Costs:"
+
+  document.getElementById("workerFood").innerText = (totalWorkers * 10).toString()
+
+  document.getElementById("workerGold").innerText = (totalWorkers * 20).toString()
+
   upgradeCostChecks()
   hireCostChecks()
   sellFoodCheck()
@@ -84,22 +97,34 @@ function drawTemplatesAndFunctions() {
   lumberjackHireTemplate()
 }
 
+//#endregion
+
+//#region COST CHECKS
+
 //Checks if the user has enough gold to allow the option to purchase an upgrade
 function upgradeCostChecks() {
-  let foodUpgradeCost = costCaculator(user.mods.food)
-  let oreUpgradeCost = costCaculator(user.mods.ore)
-  let woodUpgradeCost = costCaculator(user.mods.wood)
-  let elemFood = document.getElementById
-    ("foodUpgrade")
+  let foodUpgradeCostGold = goldCostCalculator(user.mods.food)
+  let oreUpgradeCostGold = goldCostCalculator(user.mods.ore)
+  let woodUpgradeCostGold = goldCostCalculator(user.mods.wood)
+
+  let foodUpgradeCostOre = oreCostCalculator(user.mods.food)
+  let oreUpgradeCostOre = oreCostCalculator(user.mods.ore)
+  let woodUpgradeCostOre = oreCostCalculator(user.mods.wood)
+
+  let foodUpgradeCostWood = woodCostCalculator(user.mods.food)
+  let oreUpgradeCostWood = woodCostCalculator(user.mods.ore)
+  let woodUpgradeCostWood = woodCostCalculator(user.mods.wood)
+
+  let elemFood = document.getElementById("foodUpgrade")
   let elemOre = document.getElementById("oreUpgrade")
   let elemWood = document.getElementById("woodUpgrade")
-  if (user.gold >= foodUpgradeCost) {
+  if (user.gold >= foodUpgradeCostGold && user.ore >= foodUpgradeCostOre && user.wood >= foodUpgradeCostWood) {
     elemFood.removeAttribute("disabled")
   } else elemFood.setAttribute("disabled", "")
-  if (user.gold >= oreUpgradeCost) {
+  if (user.gold >= oreUpgradeCostGold && user.ore >= oreUpgradeCostOre && user.wood >= oreUpgradeCostWood) {
     elemOre.removeAttribute("disabled")
   } else elemOre.setAttribute("disabled", "")
-  if (user.gold >= woodUpgradeCost) {
+  if (user.gold >= woodUpgradeCostGold && user.ore >= woodUpgradeCostOre && user.wood >= woodUpgradeCostWood) {
     elemWood.removeAttribute("disabled")
   } else elemWood.setAttribute("disabled", "")
 }
@@ -110,10 +135,10 @@ function hireCostChecks() {
   let elemHunter = document.getElementById("hunter")
   let elemMiner = document.getElementById("miner")
   let elemLumberjack = document.getElementById("lumberjack")
-  let traderHireCost = costCaculator(idle.gold)
-  let hunterHireCost = costCaculator(idle.food)
-  let minerHireCost = costCaculator(idle.ore)
-  let lumberjackHireCost = costCaculator(idle.wood)
+  let traderHireCost = goldCostCalculator(idle.gold)
+  let hunterHireCost = goldCostCalculator(idle.food)
+  let minerHireCost = goldCostCalculator(idle.ore)
+  let lumberjackHireCost = goldCostCalculator(idle.wood)
   if (user.gold >= traderHireCost) {
     elemTrader.removeAttribute("disabled")
   } else elemTrader.setAttribute("disabled", "")
@@ -127,6 +152,28 @@ function hireCostChecks() {
     elemLumberjack.removeAttribute("disabled")
   } else elemLumberjack.setAttribute("disabled", "")
 }
+
+//#endregion
+
+//#region COST CALCULATORS
+//Calculates the gold cost of an upgrade or hire
+function goldCostCalculator(num) {
+  let cost = 200 + (200 * num * num)
+  return cost
+}
+
+//Calculate the ore cost of an upgrade
+function oreCostCalculator(num) {
+  let cost = 50 + (200 * num * num)
+  return cost
+}
+
+//Calculates the wood cost of an upgrade
+function woodCostCalculator(num) {
+  let cost = 100 + (200 * num * num)
+  return cost
+}
+//#endregion
 
 //#region SELLING
 
@@ -295,65 +342,65 @@ function sellResources(resource, num) {
 
 //#endregion
 
-//Manual click to add resources
-function collect(resource) {
-  if (resource == "food") {
-    user.food += user.mods.food + 1
-  } else if (resource == "ore") {
-    user.ore += user.mods.ore + 1
-  } else if (resource == "wood") {
-    user.wood += user.mods.wood + 1
-  }
-  draw()
-}
-
-//TODO UPDATE RESOURCE MODIFIERS
+//#region PURCHASES
 //Increases number of resources gained per click
-function upgrade(resource, num) {
+function upgrade(resource, numGold, numOre, numWood) {
   let currentFoodRate = user.mods.food
   let currentOreRate = user.mods.ore
   let currentWoodRate = user.mods.wood
   if (resource == "food") {
     if (currentFoodRate == 0) {
       user.mods.food++
-      user.gold -= num
+      user.gold -= numGold
+      user.ore -= numOre
+      user.wood -= numWood
       upgradeCounter.click.food++
     } else {
       user.mods.food += currentFoodRate
-      user.gold -= num
+      user.gold -= numGold
+      user.ore -= numOre
+      user.wood -= numWood
       upgradeCounter.click.food++
+    }
+    if (upgradeCounter.click.food >= upgradeArr.length) {
+      maxFood++
     }
   } else if (resource == "ore") {
     if (currentOreRate == 0) {
       user.mods.ore++
-      user.gold -= num
+      user.gold -= numGold
+      user.ore -= numOre
+      user.wood -= numWood
       upgradeCounter.click.ore++
     } else {
       user.mods.ore += currentOreRate
-      user.gold -= num
+      user.gold -= numGold
+      user.ore -= numOre
+      user.wood -= numWood
       upgradeCounter.click.ore++
+    }
+    if (upgradeCounter.click.ore >= upgradeArr.length) {
+      maxOre++
     }
   } else if (resource == "wood") {
     if (currentWoodRate == 0) {
       user.mods.wood++
-      user.gold -= num
+      user.gold -= numGold
+      user.ore -= numOre
+      user.wood -= numWood
       upgradeCounter.click.wood++
     } else {
       user.mods.wood += currentWoodRate
-      user.gold -= num
+      user.gold -= numGold
+      user.ore -= numOre
+      user.wood -= numWood
       upgradeCounter.click.wood++
+    }
+    if (upgradeCounter.click.wood >= upgradeArr.length) {
+      maxWood++
     }
   }
   draw()
-}
-
-//Handles increasing resources per second
-function idleCollection() {
-  user.gold += idle.gold * 5
-  user.food += idle.food
-  user.ore += idle.ore
-  user.wood += idle.wood
-  drawUserInfo()
 }
 
 //Increases amount of resources gained per second
@@ -367,104 +414,165 @@ function hire(resources, num) {
       idle.gold++
       user.gold -= num
       upgradeCounter.idle.gold++
+      totalWorkers++
     } else {
       idle.gold += currentGoldRate
       user.gold -= num
       upgradeCounter.idle.gold++
+      totalWorkers++
     }
   } else if (resources == "food") {
     if (currentFoodRate == 0) {
       idle.food++
       user.gold -= num
       upgradeCounter.idle.food++
+      totalWorkers++
     } else {
       idle.food += currentFoodRate
       user.gold -= num
       upgradeCounter.idle.food++
+      totalWorkers++
     }
   } else if (resources == "ore") {
     if (currentOreRate == 0) {
       idle.ore++
       user.gold -= num
       upgradeCounter.idle.ore++
+      totalWorkers++
     } else {
       idle.ore += currentOreRate
       user.gold -= num
       upgradeCounter.idle.ore++
+      totalWorkers++
     }
   } else if (resources == "wood") {
     if (currentWoodRate == 0) {
       idle.wood++
       user.gold -= num
       upgradeCounter.idle.wood++
+      totalWorkers++
     } else {
       idle.wood += currentWoodRate
       user.gold -= num
       upgradeCounter.idle.wood++
+      totalWorkers++
     }
   }
   draw()
 }
+//#endregion
 
-//Calculates the cost of an upgrade or hire
-function costCaculator(num) {
-  let cost = 200 + (200 * num * num)
-  return cost
+//#region RESOURCE ADDING
+//Manual click to add resources
+function collect(resource) {
+  if (resource == "food") {
+    user.food += user.mods.food + 1
+  } else if (resource == "ore") {
+    user.ore += user.mods.ore + 1
+  } else if (resource == "wood") {
+    user.wood += user.mods.wood + 1
+  }
+  draw()
 }
 
-//Caculate the cost of upkeep for each idle resource gatherer
+//Handles increasing resources per second
+function idleCollection() {
+  user.gold += idle.gold * 5
+  user.food += idle.food
+  user.ore += idle.ore
+  user.wood += idle.wood
+  timerCounter++
+  if (timerCounter == 30) {
+    workerUpkeep()
+    timerCounter = 0
+  }
+  drawUserInfo()
+}
+//#endregion
 
+//Caculate the cost of upkeep for each idle resource gatherer
+function workerUpkeep() {
+  if (user.gold < totalWorkers * 20) {
+    workerReset()
+    user.gold = 0
+    drawTemplatesAndFunctions()
+  }
+  if (user.food < totalWorkers * 10) {
+    workerReset()
+    user.food = 0
+    drawTemplatesAndFunctions()
+  }
+  if (user.gold >= totalWorkers * 20 && user.food >= totalWorkers * 10) {
+    user.gold -= (totalWorkers * 20)
+    user.food -= (totalWorkers * 10)
+  }
+}
+
+//Reset all workers to 0 if user doesn't have enough resources to pay the upkeep costs
+function workerReset() {
+  idle.gold = 0
+  idle.food = 0
+  idle.ore = 0
+  idle.wood = 0
+  totalWorkers = 0
+}
 
 //#region TEMPLATES
 function foodUpgradeTemplate() {
-  let cost = costCaculator(user.mods.food)
+  let cost = goldCostCalculator(user.mods.food)
+  let oreCost = oreCostCalculator(user.mods.food)
+  let woodCost = woodCostCalculator(user.mods.food)
   let elem = document.getElementById("foodUpgradeTemplate")
-  elem.innerHTML = `<button id="foodUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('food',${cost})"> <img src="${upgradeArr[upgradeCounter.click.food]}arrow.png" alt="error loading image" class="userResourceImage"> Upgrade to ${upgradeArr[upgradeCounter.click.food]} Arrows.   Cost: ${cost} Gold</button>`
-  if (upgradeCounter.click.food == upgradeArr.length) {
-    elem.innerHTML = `<button id="foodUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('food',${cost}) disabled">MAX</button>`
+  elem.innerHTML = `<button id="foodUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('food',${cost}, ${oreCost}, ${woodCost})"> <div> <img src="${upgradeArr[upgradeCounter.click.food]}arrow.png" alt="error loading image" class="userResourceImage"> Upgrade to ${upgradeArr[upgradeCounter.click.food]} Arrows. </div><div>  Gold: ${cost} Ore ${oreCost} Wood ${woodCost}</div></button>`
+  if (upgradeCounter.click.food >= upgradeArr.length) {
+    elem.innerHTML = `<button id="foodUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('food',${cost}, ${oreCost}, ${woodCost})"><div> <img src="diamondarrow.png" alt="error loading image" class="userResourceImage">Upgrade Diamond Arrows +${maxFood}</div><div>  Gold: ${cost} Ore ${oreCost} Wood ${woodCost}</div></button>`
   }
 }
 
 function oreUpgradeTemplate() {
-  let cost = costCaculator(user.mods.ore)
+  let cost = goldCostCalculator(user.mods.ore)
+  let oreCost = oreCostCalculator(user.mods.ore)
+  let woodCost = woodCostCalculator(user.mods.ore)
   let elem = document.getElementById("oreUpgradeTemplate")
-  elem.innerHTML = `<button id="oreUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('ore',${cost})"> <img src="${upgradeArr[upgradeCounter.click.ore]}pick.png" alt="error loading image" class="userResourceImage"> Upgrade to ${upgradeArr[upgradeCounter.click.ore]} Pick.   Cost: ${cost} Gold</button>`
-  if (upgradeCounter.click.ore == upgradeArr.length) {
-    elem.innerHTML = `<button id="oreUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('food',${cost}) disabled">MAX</button>`
+  elem.innerHTML = `<button id="oreUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('ore',${cost}, ${oreCost}, ${woodCost})"> <img src="${upgradeArr[upgradeCounter.click.ore]}pick.png" alt="error loading image" class="userResourceImage"> Upgrade to ${upgradeArr[upgradeCounter.click.ore]} Pick. </div><div>  Gold: ${cost} Ore ${oreCost} Wood ${woodCost}</button>`
+  if (upgradeCounter.click.ore >= upgradeArr.length) {
+    elem.innerHTML = `<button id="oreUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('ore',${cost}, ${oreCost}, ${woodCost})"><div> <img src="diamondpick.png" alt="error loading image" class="userResourceImage">Upgrade Diamond Pick +${maxOre}</div><div>  Gold: ${cost} Ore ${oreCost} Wood ${woodCost}</div></button>`
   }
 }
 
 function woodUpgradeTemplate() {
-  let cost = costCaculator(user.mods.wood)
+  let cost = goldCostCalculator(user.mods.wood)
+  let oreCost = oreCostCalculator(user.mods.wood)
+  let woodCost = woodCostCalculator(user.mods.wood)
   let elem = document.getElementById("woodUpgradeTemplate")
-  elem.innerHTML = `<button id="woodUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('wood',${cost})"> <img src="${upgradeArr[upgradeCounter.click.wood]}axe.png" alt="error loading image" class="userResourceImage"> Upgrade to ${upgradeArr[upgradeCounter.click.wood]} Axe.   Cost: ${cost} Gold</button>`
-  if (upgradeCounter.click.wood == upgradeArr.length) {
-    elem.innerHTML = `<button id="woodUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('food',${cost}) disabled">MAX</button>`
+  elem.innerHTML = `<button id="woodUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('wood',${cost}, ${oreCost}, ${woodCost})"> <img src="${upgradeArr[upgradeCounter.click.wood]}axe.png" alt="error loading image" class="userResourceImage"> Upgrade to ${upgradeArr[upgradeCounter.click.wood]} Axe. </div><div>  Gold: ${cost} Ore ${oreCost} Wood ${woodCost}</button>`
+  if (upgradeCounter.click.wood >= upgradeArr.length) {
+    elem.innerHTML = `<button id="woodUpgrade" class="btn btn-primary text-shadow-black" onclick="upgrade('wood',${cost}, ${oreCost}, ${woodCost})"><div> <img src="diamondaxe.png" alt="error loading image" class="userResourceImage">Upgrade Diamond Axe +${maxWood}</div><div>  Gold: ${cost} Ore ${oreCost} Wood ${woodCost}</div></button>`
   }
 }
 
 function traderHireTemplate() {
   let elem = document.getElementById("traderHireTemplate")
-  let cost = costCaculator(idle.gold)
-  elem.innerHTML = `<button class="btn btn-primary text-shadow-black" onclick="hire('gold', ${cost})" id="trader"> <img src="trader.png" alt="error loading image" class="userResourceImage"> <div> Hire a trader</div><div> Cost: ${cost}</div></button>`
+  let cost = goldCostCalculator(idle.gold)
+  elem.innerHTML = `<button class="btn btn-primary text-shadow-black" onclick="hire('gold', ${cost})" id="trader"> <img src="trader.png" alt="error loading image" class="userResourceImage"> <div> Hire a trader</div><div> Cost: ${cost} Gold</div></button>`
 }
 
 function hunterHireTemplate() {
   let elem = document.getElementById("hunterHireTemplate")
-  let cost = costCaculator(idle.food)
-  elem.innerHTML = `<button class="btn btn-primary text-shadow-black" onclick="hire('food', ${cost})" id="hunter"> <img src="hunter.png" alt="error loading image" class="userResourceImage"> <div> Hire a hunter</div><div> Cost: ${cost}</div></button>`
+  let cost = goldCostCalculator(idle.food)
+  elem.innerHTML = `<button class="btn btn-primary text-shadow-black" onclick="hire('food', ${cost})" id="hunter"> <img src="hunter.png" alt="error loading image" class="userResourceImage"> <div> Hire a hunter</div><div> Cost: ${cost} Gold</div></button>`
 }
 
 function minerHireTemplate() {
   let elem = document.getElementById("minerHireTemplate")
-  let cost = costCaculator(idle.ore)
-  elem.innerHTML = `<button class="btn btn-primary text-shadow-black" onclick="hire('ore', ${cost})" id="miner"> <img src="miner.png" alt="error loading image" class="userResourceImage"> <div>Hire a miner</div><div> Cost: ${cost}</div></button>`
+  let cost = goldCostCalculator(idle.ore)
+  elem.innerHTML = `<button class="btn btn-primary text-shadow-black" onclick="hire('ore', ${cost})" id="miner"> <img src="miner.png" alt="error loading image" class="userResourceImage"> <div>Hire a miner</div><div> Cost: ${cost} Gold</div></button>`
 }
 
 function lumberjackHireTemplate() {
   let elem = document.getElementById("lumberjackHireTemplate")
-  let cost = costCaculator(idle.wood)
-  elem.innerHTML = `<button class="btn btn-primary text-shadow-black" onclick="hire('wood', ${cost})" id="lumberjack"> <img src="lumberjack.png" alt="error loading image" class="userResourceImage"><div> Hire a lumberjack</div><div> Cost: ${cost}</div></button>`
+  let cost = goldCostCalculator(idle.wood)
+  elem.innerHTML = `<button class="btn btn-primary text-shadow-black" onclick="hire('wood', ${cost})" id="lumberjack"> <img src="lumberjack.png" alt="error loading image" class="userResourceImage"><div> Hire a lumberjack</div><div> Cost: ${cost} Gold</div></button>`
 }
 
 //#endregion
